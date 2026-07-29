57ef69194e555602dd22e2b1880b1592715f5104
Saksham Upadhyay <sakshamupadhyay.dev@gmail.com>
2026-07-29 15:02:07 +0530

    Correct the file structure

---
+++ LearningCodes/JavaScript_with_HTML/first.js
@@ -0,0 +1,15 @@
+console.log("Hello Zom");   // Message on console tab (on inspect)
+console.log("It's a new program, a new language");
+
+const name = "Zom";
+const fullName = "Real Zom"
+let age = 19;
+let radius = 14.2;
+let isAvailable= true;
+
+console.log(name)
+console.log(fullName)
+console.log(age)
+console.log(radius)
+console.log(isAvailable)
+console.log(typeof(isAvailable))
