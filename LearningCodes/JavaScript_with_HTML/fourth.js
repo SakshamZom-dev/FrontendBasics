b484c539bb10e74c1ae7fe71fba702e96c7ce5f4
Saksham Upadhyay <sakshamupadhyay.dev@gmail.com>
2026-08-08 00:38:21 +0530

    Add more of DOM manipulation synatx

--- LearningCodes/JavaScript_with_HTML/fourth.js
+++ LearningCodes/JavaScript_with_HTML/fourth.js
@@ -26,3 +26,44 @@ console.dir(div1);
 console.log(div1.innerText);                                // Returns visible text only (respects CSS styling like display: none)
 console.log(div1.textContent);                              // Returns all text, including hidden elements and raw line breaks
 console.log(div1.innerHTML);                                // Returns full HTML structure markup inside the div as a string
+
+// // ...........................................
+
+let h3 = document.querySelector("h3")
+console.log(h3);
+console.log(h3.getAttribute("id"));                         // attributes get applied to first one, not everyone
+
+// h3.setAttribute("class", "Heading3");                       // can make/change attributes
+// console.log(h3);
+
+let h3_style = h3.style;                                    // get style
+console.log(h3_style);
+
+div1.style.backgroundColor = "yellowgreen";                 // change style
+// div1.style.visibility = "hidden";
+
+// // ...........................................
+
+let newBut1 = document.createElement("button");              // create new element
+newBut1.innerText = "Button 1!";
+console.log(newBut1);
+div1.append(newBut1);                                        // add the element to html (inside, later)
+
+let newBut2 = document.createElement("button");
+newBut2.innerText = "Button 2!";
+div1.prepend(newBut2);                                      // add the element to html (inside, before)
+
+let newBut3 = document.createElement("button");
+newBut3.innerText = "Button 3!";
+div1.before(newBut3);                                       // add the element to html (outside, before)
+
+let newBut4 = document.createElement("button");
+newBut4.innerText = "Button 4!";
+div1.after(newBut4);                                        // add the element to html (outside, after)
+
+let newHeading = document.createElement("h2");
+newHeading.innerHTML = "<i>Hello Everyone to H2</i>";
+document.querySelector("body").prepend(newHeading);
+
+let para2 = document.querySelector("#para2");
+para2.remove();                                             // delete/remove an element
